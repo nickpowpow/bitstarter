3ab5795263e95fa96f65d6b44ed3ab38bde15689
Nick Matthews <matthnick@gmail.com>
2013-07-12 03:44:36 +0000

    Changed file reading

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var index_buffer = fs.readFileSync("index.html");
+var index_string = index_buffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(index_string);
 });
 
 var port = process.env.PORT || 5000;
