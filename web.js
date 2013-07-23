c701a12622e92670cae835c8b2947429aa41bf03
Nick Matthews <matthnick@gmail.com>
2013-07-23 03:10:47 +0000

    added web.js

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-
-var index_buffer = fs.readFileSync("index.html");
-var index_string = index_buffer.toString();
+var htmlfile= "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index_string);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(index_string);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
